bf001ea1ab82894b349574a2d524c3841a5cb7ed
davidv <davidvazquezgit@gmail.com>
2022-10-10 14:47:20 -0700

    finish Lab

--- src/FloorPlan.jsx
+++ src/FloorPlan.jsx
@@ -1,7 +1,21 @@
-const FloorPlan = (props) => {
+import Kitchen from "./Kitchen";
+import LivingRoom from "./LivingRoom";
+import Bedroom from "./Bedroom";
+import Bath from "./Bath";
+
+const FloorPlan = () => {
   return ( 
-    
+    <>
+      <Bedroom bedNum={1}/>
+      <Kitchen />
+      <Bath size={'Full'}/>
+      <LivingRoom />
+      <Bedroom bedNum={2}/>
+      <Bath size={'Half'}/>
+      <Bedroom bedNum={3}/>
+    </>
   );
 }
 
 export default FloorPlan;
+
